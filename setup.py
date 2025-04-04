4ccc19279d5b039ba4ba580c0fa417d2d91232a7
seanie5011 <seanie500@hotmail.com>
2025-04-04 11:30:41 +0200

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="detach_rocket",
-    version="0.0.2",
+    version="0.0.3",
     author="Gonzalo Uribarri & Federico Barone",
     description="Sequential Feature Detachment for Random Convolutional Kernel models.",
     long_description_content_type="text/markdown",
